947f2ed639e02685a585971d9f27bdb944eb4b1e
vader1359 <vader1359@gmail.com>
2017-08-01 16:04:10 +0700

    Re add data SEED

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Seeding data"
+
+# Delete old data and create the same test data
+if Photo.count > 0
+  puts "WARNING: there is existing data."
+  puts "To re-seed, first go to rails console and type 'Photo.delte_all'"
+  puts "For heroku, type 'heroku run rails console'"
+  exit
+end
+
+Photo.create!(url: "https://pbs.twimg.com/media/Clusm_JWYAEj2H-.jpg", username: "mjackson", likes_count: 1234)
+Photo.create!(url: "https://pbs.twimg.com/profile_images/720767103712645122/6XEBAXLj.jpg", username: "taylor", likes_count: 232)
+Photo.create!(url: "http://i.imgur.com/vrU6Kmu.jpg", username: "gamer", likes_count: 11)
+
+puts "Seeded #{Photo.count} photos."
